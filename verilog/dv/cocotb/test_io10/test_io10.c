4ac6632ac1a3d793951d94d4a8380ea84790957b
M0stafaRady <mostafa.rady@efabless.com>
2023-08-23 09:58:38 +0300

    fix test_io10 C code

--- verilog/dv/cocotb/test_io10/test_io10.c
+++ verilog/dv/cocotb/test_io10/test_io10.c
@@ -4,9 +4,6 @@ void main(){
     // dummy code to keep the cpu running 
     int x = 0;
     for (int i = 0; i < 100000; i++){
-        if (i%2 == 0)
-            x = x+2;
-        else
-            x = x+1;
+       x++;
     }
 }
